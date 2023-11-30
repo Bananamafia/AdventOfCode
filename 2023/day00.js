cdc07ffe8b7d5d29558a16724dc59226c01332c3
Bananamafia <63409017+Bananamafia@users.noreply.github.com>
2023-11-30 22:45:19 +0100

    Add script to read file line by line

---
+++ 2023/day00.js
@@ -0,0 +1,22 @@
+const rl = require("node:readline");
+const fs = require('node:fs');
+
+const stream = fs.createReadStream("data/data.txt", "utf-8");
+const rl = readLine.createInterface(stream);
+
+let value1 = 0;
+let value2 = 0;
+
+rl.on("line", (line) => {
+
+    if (line == '') {
+
+    }
+    else {
+        
+    }
+})
+
+rl.on("close", () => {
+    console.log();
+});
