e68583be5634530b0f31e0778c37ef7ab4cb2534
Bananamafia <63409017+Bananamafia@users.noreply.github.com>
2023-12-03 20:28:12 +0100

    Solution day03-2

--- 2023/day03.js
+++ 2023/day03.js
@@ -28,47 +28,98 @@ function isCharacterSymbol(character) {
         return true;
 }
 
+function isCharacterGear(character) {
+    return character == "*";
+}
+
 let lastLine;
 let sum = 0;
+const processedLines = [];
+
+function getpartNumbers() {
+    rl.on("line", (line) => {
+        const currentLine = new ProcessedLine(line);
+
+        if (lastLine) {
+            const additionalPartNumbersOfPreviousRow = lastLine.getPartNumbersWithSymbolsOfOtherRow(currentLine.symbolIndexes);
+
+            for (let i = 0; i < additionalPartNumbersOfPreviousRow.length; i++) {
+                sum += additionalPartNumbersOfPreviousRow[i];
+            }
+
+            const partNumbersFromSymbolsOfPreviousRow = currentLine.getPartNumbersWithSymbolsOfOtherRow(lastLine.symbolIndexes);
+            for (let i = 0; i < partNumbersFromSymbolsOfPreviousRow.length; i++) {
+                sum += partNumbersFromSymbolsOfPreviousRow[i];
+            }
+        }
+
+        const currentLinePartNumbers = currentLine.getPartNumbersWithSymbolsOfCurrentRow();
+        for (let i = 0; i < currentLinePartNumbers.length; i++) {
+            sum += currentLinePartNumbers[i];
+        }
+
+        lastLine = currentLine;
+    });
+}
 
 rl.on("line", (line) => {
-    const currentLine = new processedLine(line);
-
-    if (lastLine) {
-        const additionalPartNumbersOfPreviousRow = lastLine.getPartNumbersWithSymbolsOfOtherRow(currentLine.symbolIndexes);
-
-        for (let i = 0; i < additionalPartNumbersOfPreviousRow.length; i++) {
-            sum += additionalPartNumbersOfPreviousRow[i];
-        }
-
-        const partNumbersFromSymbolsOfPreviousRow = currentLine.getPartNumbersWithSymbolsOfOtherRow(lastLine.symbolIndexes);
-        for (let i = 0; i < partNumbersFromSymbolsOfPreviousRow.length; i++) {
-            sum += partNumbersFromSymbolsOfPreviousRow[i];
-        }
-    }
-
-    const currentLinePartNumbers = currentLine.getPartNumbersWithSymbolsOfCurrentRow();
-    for (let i = 0; i < currentLinePartNumbers.length; i++) {
-        sum += currentLinePartNumbers[i];
-    }
-
-    lastLine = currentLine;
+    processedLines.push(new ProcessedLine(line));
 });
 
 rl.on("close", () => {
+    for (let processedLineIndex = 0; processedLineIndex < processedLines.length; processedLineIndex++) {
+        const processedLine = processedLines[processedLineIndex];
+    
+        for (let i = 0; i < processedLine.gearIndexes.length; i++) {
+            const gearIndex = processedLine.gearIndexes[i];
+            const connectedNumbers = [];
+    
+            for (let j = 0; j < processedLine.potentialPartNumbers.length; j++) {
+                const number = processedLine.potentialPartNumbers[j];
+                if (number.isConnectedTo(gearIndex)) {
+                    connectedNumbers.push(number.getNumber());
+                }
+            }
+    
+            if (processedLineIndex > 0) {
+                const previousLine = processedLines[processedLineIndex - 1];
+                for (let j = 0; j < previousLine.potentialPartNumbers.length; j++) {
+                    const number = previousLine.potentialPartNumbers[j];
+                    if (number.isConnectedTo(gearIndex)) {
+                        connectedNumbers.push(number.getNumber());
+                    }
+                }
+            }
+    
+            if(processedLineIndex < processedLines.length){
+                const nextLine = processedLines[processedLineIndex + 1];
+                for (let j = 0; j < nextLine.potentialPartNumbers.length; j++) {
+                    const number = nextLine.potentialPartNumbers[j];
+                    if (number.isConnectedTo(gearIndex)) {
+                        connectedNumbers.push(number.getNumber());
+                    }
+                }
+            }
+    
+            if (connectedNumbers.length == 2) {
+                sum += connectedNumbers[0] * connectedNumbers[1];
+            }
+        }
+    }
+    
     console.log(sum);
 });
 
-
-class processedLine {
+class ProcessedLine {
     symbolIndexes;
     potentialPartNumbers = [];
+    gearIndexes = [];
 
     constructor(line) {
         this.symbolIndexes = [];
         this.potentialPartNumbers = [];
 
-        let lastPotentialNumber = new potentialPartNumber();
+        let lastPotentialNumber = new PotentialPartNumber();
 
         for (let index = 0; index < line.length; index++) {
             const character = line[index];
@@ -79,10 +130,13 @@ class processedLine {
                 }
                 else {
                     this.potentialPartNumbers.push(lastPotentialNumber);
-                    lastPotentialNumber = new potentialPartNumber();
+                    lastPotentialNumber = new PotentialPartNumber();
                     lastPotentialNumber.startIndex = index;
                     lastPotentialNumber.numberString = character;
                 }
+            }
+            else if (isCharacterGear(character)) {
+                this.gearIndexes.push(index);
             }
             else if (isCharacterSymbol(character)) {
                 this.symbolIndexes.push(index);
@@ -120,7 +174,7 @@ class processedLine {
     }
 }
 
-class potentialPartNumber {
+class PotentialPartNumber {
     startIndex = -1;
     numberString = "";
 
