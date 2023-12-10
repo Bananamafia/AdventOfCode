1805c3c8d60cb5176047c29133523d553d6f45df
Bananamafia <63409017+Bananamafia@users.noreply.github.com>
2023-12-10 15:51:45 +0100

    Solution day10-2 (kinda messy)

--- 2023/day10.js
+++ 2023/day10.js
@@ -16,17 +16,33 @@ const NORTH_WEST = "J";
 const SOUTH_WEST = "7";
 const SOUTH_EAST = "F";
 const GROUND = ".";
+const LEFT = "left";
+const RIGHT = "right";
+const TOP = "top";
+const BOTTOM = "bottom";
 
 let pipeDiagram = [];
 let startCoordinate;
+const pipeCoordinates = new Set();
 let pipeCount = 0;
+let outerSide = "";
+let groundLeftFromPipe = new Set();
+let groundRightFromPipe = new Set();
 
 
 function main() {
     transposePipeDiagram();
     traversePipe();
+    traversePipeWithGroundsets();
 
     console.log(getMaxDistance(pipeCount));
+
+    if (outerSide == RIGHT) {
+        console.log(groundLeftFromPipe.size);
+    }
+    else if (outerSide == LEFT) {
+        console.log(groundRightFromPipe.size);
+    }
 }
 
 function processLine(line) {
@@ -63,8 +79,258 @@ function traversePipe() {
         const tempCurrentPipe = new PipeCoordinate(currentCoordinate.x, currentCoordinate.y);
         currentCoordinate.SetToNext(previousCoordinate, currentPipeType);
         previousCoordinate = tempCurrentPipe;
+
+        pipeCoordinates.add(PipeCoordinate.GetPipeId(currentCoordinate.x, currentCoordinate.y));
         pipeCount++;
     } while (currentPipeType != START_LOCATION);
+}
+
+function traversePipeWithGroundsets() {
+
+    let previousCoordinate = startCoordinate;
+    let currentCoordinate = new PipeCoordinate(startCoordinate.x, startCoordinate.y + 1);
+
+    let currentPipeType;
+    do {
+        currentPipeType = pipeDiagram[currentCoordinate.y][currentCoordinate.x];
+
+        fillGroundSets(previousCoordinate, currentCoordinate, currentPipeType);
+
+        const tempCurrentPipe = new PipeCoordinate(currentCoordinate.x, currentCoordinate.y);
+        currentCoordinate.SetToNext(previousCoordinate, currentPipeType);
+        previousCoordinate = tempCurrentPipe;
+    } while (currentPipeType != START_LOCATION);
+}
+
+function fillGroundSets(previousCoordninate, currentCoordinate, currentPipeType) {
+    const directionComingFrom = PipeCoordinate.GetDirectionComingFrom(previousCoordninate, currentCoordinate);
+
+    const leftIds = getGroundLeftFromPipe(currentCoordinate);
+    const rightIds = getGroundRightFromPipe(currentCoordinate);
+    const topIds = getGroundAbovePipe(currentCoordinate);
+    const bottomIds = getGroundBelowPipe(currentCoordinate);
+
+    switch (currentPipeType) {
+        case GROUND:
+            throw Error("Can't find direction from ground.");
+        case NORTH_SOUTH:
+            if (directionComingFrom == TOP) {
+                groundRightFromPipe = new Set([...groundRightFromPipe, ...leftIds.ids])
+                if (leftIds.wasOuterSide) {
+                    outerSide = RIGHT;
+                }
+
+                groundLeftFromPipe = new Set([...groundLeftFromPipe, ...rightIds.ids]);
+                if (rightIds.wasOuterSide) {
+                    outerSide == LEFT;
+                }
+            }
+            else {
+                groundRightFromPipe = new Set([...groundRightFromPipe, ...rightIds.ids])
+                if (rightIds.wasOuterSide) {
+                    outerSide = RIGHT;
+                }
+
+                groundLeftFromPipe = new Set([...groundLeftFromPipe, ...leftIds.ids]);
+                if (leftIds.wasOuterSide) {
+                    outerSide == LEFT;
+                }
+            }
+            break;
+        case EAST_WEST:
+            if (directionComingFrom == LEFT) {
+                groundLeftFromPipe = new Set([...groundLeftFromPipe, ...topIds.ids])
+                if (topIds.wasOuterSide) {
+                    outerSide = LEFT;
+                }
+
+                groundRightFromPipe = new Set([...groundRightFromPipe, ...bottomIds.ids]);
+                if (bottomIds.wasOuterSide) {
+                    outerSide == RIGHT;
+                }
+            }
+            else {
+                groundLeftFromPipe = new Set([...groundLeftFromPipe, ...bottomIds.ids])
+                if (bottomIds.wasOuterSide) {
+                    outerSide = LEFT;
+                }
+
+                groundRightFromPipe = new Set([...groundRightFromPipe, ...topIds.ids]);
+                if (topIds.wasOuterSide) {
+                    outerSide == RIGHT;
+                }
+            }
+            break;
+        case NORTH_EAST:
+            if (directionComingFrom == TOP) {
+                groundRightFromPipe = new Set([...groundRightFromPipe, ...leftIds.ids, ...bottomIds.ids]);
+                if (leftIds.wasOuterSide || bottomIds.wasOuterSide) {
+                    outerSide == RIGHT;
+                }
+            }
+            else {
+                groundLeftFromPipe = new Set([...groundLeftFromPipe, ...leftIds.ids, ...bottomIds.ids]);
+                if (leftIds.wasOuterSide || bottomIds.wasOuterSide) {
+                    outerSide == LEFT;
+                }
+            }
+            break;
+        case NORTH_WEST:
+            if (directionComingFrom == TOP) {
+                groundLeftFromPipe = new Set([...groundLeftFromPipe, ...rightIds.ids, ...bottomIds.ids]);
+                if (rightIds.wasOuterSide || bottomIds.wasOuterSide) {
+                    outerSide == LEFT;
+                }
+            }
+            else {
+                groundRightFromPipe = new Set([...groundRightFromPipe, ...rightIds.ids, ...bottomIds.ids]);
+                if (rightIds.wasOuterSide || bottomIds.wasOuterSide) {
+                    outerSide == RIGHT;
+                }
+            }
+            break;
+        case SOUTH_WEST:
+            if (directionComingFrom == BOTTOM) {
+                groundRightFromPipe = new Set([...groundRightFromPipe, ...rightIds.ids, ...topIds.ids]);
+                if (rightIds.wasOuterSide || topIds.wasOuterSide) {
+                    outerSide == RIGHT;
+                }
+            }
+            else {
+                groundLeftFromPipe = new Set([...groundLeftFromPipe, ...rightIds.ids, ...topIds.ids]);
+                if (rightIds.wasOuterSide || topIds.wasOuterSide) {
+                    outerSide == LEFT;
+                }
+            }
+            break;
+        case SOUTH_EAST:
+            if (directionComingFrom == BOTTOM) {
+                groundLeftFromPipe = new Set([...groundLeftFromPipe, ...leftIds.ids, ...topIds.ids]);
+                if (leftIds.wasOuterSide || topIds.wasOuterSide) {
+                    outerSide == LEFT;
+                }
+            }
+            else {
+                groundRightFromPipe = new Set([...groundRightFromPipe, ...leftIds.ids, ...topIds.ids]);
+                if (leftIds.wasOuterSide || topIds.wasOuterSide) {
+                    outerSide == RIGHT;
+                }
+            }
+            break;
+    }
+}
+
+function getGroundAbovePipe(currentCoordinate) {
+    const groundIds = new Set();
+
+    let currentAboveIndex = currentCoordinate.y - 1;
+    while (currentAboveIndex >= -1) {
+        try {
+            currentPipeType = pipeDiagram[currentAboveIndex][currentCoordinate.x];
+            
+            const isNotPartOfPipe = !pipeCoordinates.has(PipeCoordinate.GetPipeId(currentCoordinate.x, currentAboveIndex));
+
+            if (currentPipeType && isNotPartOfPipe) {
+                groundIds.add(PipeCoordinate.GetPipeId(currentCoordinate.x, currentAboveIndex));
+                currentAboveIndex--;
+            }
+            else if (!currentPipeType) {
+                return { ids: groundIds, wasOuterSide: true };
+            }
+            else {
+                break;
+            }
+        } catch {
+            return { ids: groundIds, wasOuterSide: true };
+        }
+    }
+
+    return { ids: groundIds, wasOuterSide: false };
+}
+
+function getGroundBelowPipe(currentCoordinate) {
+    const groundIds = new Set();
+
+    let currentBelowIndex = currentCoordinate.y + 1;
+    while (currentBelowIndex <= pipeDiagram.length) {
+        try {
+            currentPipeType = pipeDiagram[currentBelowIndex][currentCoordinate.x];
+
+            const isNotPartOfPipe = !pipeCoordinates.has(PipeCoordinate.GetPipeId(currentCoordinate.x, currentBelowIndex));
+
+            if (currentPipeType && isNotPartOfPipe) {
+                groundIds.add(PipeCoordinate.GetPipeId(currentCoordinate.x, currentBelowIndex));
+                currentBelowIndex++;
+            }
+            else if (!currentPipeType) {
+                return { ids: groundIds, wasOuterSide: true };
+            }
+            else {
+                break;
+            }
+        } catch {
+            return { ids: groundIds, wasOuterSide: true };
+        }
+    }
+
+    return { ids: groundIds, wasOuterSide: false };
+}
+
+function getGroundLeftFromPipe(currentCoordinate) {
+    const groundIds = new Set();
+
+    let currentLeftFromIndex = currentCoordinate.x - 1;
+    while (currentLeftFromIndex >= -1) {
+        try {
+            currentPipeType = pipeDiagram[currentCoordinate.y][currentLeftFromIndex];
+
+            const isNotPartOfPipe = !pipeCoordinates.has(PipeCoordinate.GetPipeId(currentLeftFromIndex, currentCoordinate.y));
+
+
+            if (currentPipeType && isNotPartOfPipe) {
+                groundIds.add(PipeCoordinate.GetPipeId(currentLeftFromIndex, currentCoordinate.y));
+                currentLeftFromIndex--;
+            }
+            else if (!currentPipeType) {
+                return { ids: groundIds, wasOuterSide: true };
+            }
+            else {
+                break;
+            }
+        } catch {
+            return { ids: groundIds, wasOuterSide: true };
+        }
+    }
+
+    return { ids: groundIds, wasOuterSide: false };
+}
+
+function getGroundRightFromPipe(currentCoordinate) {
+    const groundIds = new Set();
+
+    let currentRightFromIndex = currentCoordinate.x + 1;
+    while (currentRightFromIndex <= pipeDiagram[currentCoordinate.y].length) {
+        try {
+            currentPipeType = pipeDiagram[currentCoordinate.y][currentRightFromIndex];
+
+            const isNotPartOfPipe = !pipeCoordinates.has(PipeCoordinate.GetPipeId(currentRightFromIndex, currentCoordinate.y));
+
+            if (currentPipeType && isNotPartOfPipe) {
+                groundIds.add(PipeCoordinate.GetPipeId(currentRightFromIndex, currentCoordinate.y));
+                currentRightFromIndex++;
+            }
+            else if (!currentPipeType) {
+                return { ids: groundIds, wasOuterSide: true };
+            }
+            else {
+                break;
+            }
+        } catch {
+            return { ids: groundIds, wasOuterSide: true };
+        }
+    }
+
+    return { ids: groundIds, wasOuterSide: false };
 }
 
 function getMaxDistance(pipeCount) {
@@ -82,6 +348,29 @@ class PipeCoordinate {
     Update(x, y) {
         this.x = x;
         this.y = y;
+    }
+
+    static GetPipeId(x, y) {
+        const coordinate = new PipeCoordinate(x, y);
+        return JSON.stringify(coordinate);
+    }
+
+    static GetDirectionComingFrom(previous, current) {
+        if (previous.x < current.x) {
+            return LEFT;
+        }
+        else if (previous.x > current.x) {
+            return RIGHT;
+        }
+        else if (previous.y < current.y) {
+            return TOP;
+        }
+        else if (previous.y > current.y) {
+            return BOTTOM;
+        }
+        else {
+            return "UNKNWON";
+        }
     }
 
     SetToNext(previous, pipeType) {
